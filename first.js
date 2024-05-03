ccbb76c859432df168c13ebb31ea1a9016380108
sameergit123 <sameerpersonol1020@gmail.com>
2024-05-03 17:38:20 +0530

    random boolean if else switch

--- first.js
+++ first.js
@@ -1,2 +1,11 @@
-const d = new Date();
-console.log(d.getHours());
+const hour = new Date().getHours();
+let greeting;
+
+if (hour > 18) {
+  greeting = "Good day";
+} else {
+  greeting = "Good evening";
+}
+
+// Replace this line with your desired output logic (e.g., console.log or store in a variable)
+console.log(greeting);
